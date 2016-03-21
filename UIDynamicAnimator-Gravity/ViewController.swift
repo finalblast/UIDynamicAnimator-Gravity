5951cec8d45614309abd889b77d4bee85984539c
Nam (Nick) N. HUYNH <nick@enclave.vn>
2016-03-21 14:39:40 +0700

    Gravity and collision in UIDynamicAnimator

--- UIDynamicAnimator-Gravity/ViewController.swift
+++ UIDynamicAnimator-Gravity/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let bottomBoundary = "bottomBoundary"
+    var squareViews = [AnyObject]()
+    var animator: UIDynamicAnimator?
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        let colors = [UIColor.redColor(), UIColor.greenColor()]
+        var currentCenterPoint = view.center
+        let eachViewSize = CGSize(width: 50, height: 50)
+        for counter in 0 ..< 2 {
+            
+            let newView = UIView(frame: CGRectMake(0, 0, eachViewSize.width, eachViewSize.height))
+            newView.backgroundColor = colors[counter]
+            newView.center = currentCenterPoint
+            currentCenterPoint.y += eachViewSize.height + 10
+            squareViews.append(newView)
+            view.addSubview(newView)
+            
+        }
+        animator = UIDynamicAnimator(referenceView: view)
+        let gravity = UIGravityBehavior(items: squareViews)
+        animator!.addBehavior(gravity)
+        let collision = UICollisionBehavior(items: squareViews)
+        let startPoint = CGPoint(x: 0, y: view.bounds.size.height - 100)
+        let toPoint = CGPoint(x: view.bounds.size.width, y: view.bounds.size.height - 100)
+        collision.addBoundaryWithIdentifier(bottomBoundary, fromPoint: startPoint, toPoint: toPoint)
+        collision.collisionDelegate = self
+        collision.collisionMode = UICollisionBehaviorMode.Items
+        animator!.addBehavior(collision)
+        
+    }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +54,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension ViewController: UICollisionBehaviorDelegate {
+    
+    func collisionBehavior(behavior: UICollisionBehavior, beganContactForItem item: UIDynamicItem, withBoundaryIdentifier identifier: NSCopying, atPoint p: CGPoint) {
+        
+        if identifier as? String == bottomBoundary {
+            
+            UIView.animateWithDuration(1, animations: { () -> Void in
+                
+                let view = item as UIView
+                
+                view.backgroundColor = UIColor.redColor()
+                view.alpha = 0
+                view.transform = CGAffineTransformMakeScale(4, 4)
+                
+            }, completion: { (finished) -> Void in
+                
+                let view = item as UIView
+                behavior.removeItem(item)
+                view.removeFromSuperview()
+                
+            })
+            
+        }
+        
+    }
+    
+}
